3866c168f0ff2f35a9c121aa94e1ca897087dfa0
tanoscar111 <tanoscar2810@gmail.com>
2022-03-08 06:15:01 +0700

    Server

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-app.listen(5000, () => {
+const mongooses = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+mongooses
+  .connect(process.env.DB_URL)
+  .then(() => console.log("DBConnection Succcess Ahihih !!!"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.get("/api/test", () => {
+  console.log("test is Succcess ahihih");
+});
+app.listen(process.env.PORT || 5000, () => {
   console.log("backend server is running");
 });
