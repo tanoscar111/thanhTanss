215ba7f138e8b3e1253de716a4437c284c53e28b
tanoscar111 <tanoscar2810@gmail.com>
2022-03-08 13:27:06 +0700

    register

--- Server/index.js
+++ Server/index.js
@@ -3,15 +3,17 @@ const app = express();
 const mongooses = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const userRouter = require("./routes/user");
+const AuthRouter = require("./routes/auth");
 mongooses
   .connect(process.env.DB_URL)
   .then(() => console.log("DBConnection Succcess Ahihih !!!"))
   .catch((err) => {
     console.log(err);
   });
-app.get("/api/test", () => {
-  console.log("test is Succcess ahihih");
-});
+app.use(express.json());
+app.use("/", userRouter);
+app.use("/", AuthRouter);
 app.listen(process.env.PORT || 5000, () => {
   console.log("backend server is running");
 });
---
+++ Server/routes/auth.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+//Register
+
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    console.log("TCL: savedUser", savedUser);
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
