0dfcf62baa7ef6b929dad8afedf1719f22b444c8
tanoscar111 <tanoscar2810@gmail.com>
2022-04-06 22:23:59 +0700

    add test git

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGOOSEDB_UL, () => {
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-console.log("asdasd");
+console.log("tesstgit");
 
 //router
 app.use("/v1/auth", authRoute);
