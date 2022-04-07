04e6aeb3ace041bb1e0405f6ee06af08528de628
tanoscar111 <tanoscar2810@gmail.com>
2022-04-07 13:44:23 +0700

    veryfy token

--- Server/controllers/authControllers.js
+++ Server/controllers/authControllers.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
+let refreshTokens = [];
 const authControllers = {
   registerUser: async (req, res) => {
     try {
@@ -19,7 +20,7 @@ const authControllers = {
   },
   generateAccessToken: (data) => {
     return jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "36d",
+      expiresIn: "10s",
     });
   },
   generateRefeshToken: (data) => {
@@ -45,12 +46,53 @@ const authControllers = {
         const data = req.body;
         const accessToken = authControllers.generateAccessToken(data);
         const freshToken = authControllers.generateRefeshToken(data);
+        refreshTokens.push(freshToken);
+        res.cookie("refestoken", freshToken, {
+          httpOnly: true,
+          secure: false,
+          path: "/",
+          sameSite: "strict",
+        });
         const { password, ...other } = user._doc; // Loại bỏ password ra khỏi token
-        res.status(200).json({ ...other, accessToken, freshToken });
+        res.status(200).json({ ...other, accessToken });
       }
     } catch (error) {
       res.status(500).json(error);
     }
+  },
+  requestToken: async (req, res) => {
+    // lấy token từ cookie ra
+    const refestoken = req.cookies.refestoken;
+    res.status(200).json(refestoken);
+    if (!refestoken)
+      return res.status(401).json("bạn cần đăng nhập rồi mới refresh");
+    if (!refreshTokens.includes(refestoken)) {
+      return res.status(403).json("refesh token không đúng");
+    }
+    const data = req.body;
+    jwt.verify(refestoken, process.env.ACCESS_TOKEN_SECRET, (error, data) => {
+      if (error) {
+        console.log(error);
+      } //nếu không đổi sẽ tạo token mới
+      refreshTokens = refreshTokens.filter((token) => token !== refestoken);
+      const newAccessToken = authControllers.generateAccessToken(data);
+      const newRefreshTolen = authControllers.generateRefeshToken(data);
+      refreshTokens.push(newAccessToken);
+      res.cookie("refestoken", newRefreshTolen, {
+        httpOnly: true,
+        secure: false,
+        path: "/",
+        sameSite: "strict",
+      });
+      res.status(200).json({ accessToken: newAccessToken });
+    });
+  },
+  userLogout: async (req, res) => {
+    res.clearCookie("refestoken");
+    refreshTokens = refreshTokens.filter(
+      (token) => token !== req.cookies.refestoken
+    );
+    res.status(200).json("Đăng xuất thành công");
   },
 };
 module.exports = authControllers;
--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,6 +1,13 @@
 const authControllers = require("../controllers/authControllers");
+const middlewareController = require("../controllers/middlewareController");
 
 const router = require("express").Router();
 router.post("/register", authControllers.registerUser);
 router.post("/login", authControllers.loginUser);
+router.post("/refresh", authControllers.requestToken);
+router.post(
+  "/logout",
+  middlewareController.verifyToken,
+  authControllers.userLogout
+);
 module.exports = router;
